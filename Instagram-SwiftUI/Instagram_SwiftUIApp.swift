93041d1a57204e1a6e6c23aa28c7bdc2e939f704
PANKYAAA <pankajs.gaikar@gmail.com>
2021-04-03 11:28:49 +0530

    Initial Commit

---
+++ Instagram-SwiftUI/Instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Instagram-SwiftUI
+//
+//  Created by Pankaj Gaikar on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Instagram-SwiftUITests/Instagram_SwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  Instagram_SwiftUITests.swift
+//  Instagram-SwiftUITests
+//
+//  Created by Pankaj Gaikar on 03/04/21.
+//
+
+import XCTest
+@testable import Instagram_SwiftUI
+
+class Instagram_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
