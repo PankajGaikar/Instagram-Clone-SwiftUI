878a701cfea95aa86ca82ad79170e3c957969ab0
PANKYAAA <pankajs.gaikar@gmail.com>
2021-04-03 11:57:43 +0530

    Added TabView
    Added all stub views.

---
+++ Instagram-SwiftUI/Views/ReelsView.swift
@@ -0,0 +1,20 @@
+//
+//  ReelsView.swift
+//  Instagram-SwiftUI
+//
+//  Created by Pankaj Gaikar on 03/04/21.
+//
+
+import SwiftUI
+
+struct ReelsView: View {
+    var body: some View {
+        Text("Hello, World! Reels View")
+    }
+}
+
+struct ReelsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReelsView()
+    }
+}
---
+++ Instagram-SwiftUI/Views/TimeLineView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeLineView.swift
+//  Instagram-SwiftUI
+//
+//  Created by Pankaj Gaikar on 03/04/21.
+//
+
+import SwiftUI
+
+struct TimeLineView: View {
+    var body: some View {
+        Text("Hello, World! Timeline View")
+    }
+}
+
+struct TimeLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeLineView()
+    }
+}
