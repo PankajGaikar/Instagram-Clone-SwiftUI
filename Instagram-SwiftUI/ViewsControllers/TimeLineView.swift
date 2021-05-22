4708444e29cad1c3ca555b8c14bc39f2e2fe9bdf
PANKYAAA <pankajs.gaikar@gmail.com>
2021-05-22 11:20:27 +0530

    Removed navigation view. Fixes spacing issue..

--- Instagram-SwiftUI/ViewsControllers/TimeLineView.swift
+++ Instagram-SwiftUI/ViewsControllers/TimeLineView.swift
@@ -10,35 +10,39 @@ import SwiftUI
 struct TimeLineView: View {
     
     var body: some View {
-        VStack {
-            HStack {
-                Image("instagram")
-                    .resizable()
-                    .frame(width: 130, height: 45)
-                Spacer()
-                Image(systemName: "plus.square.on.square")
-                    .resizable()
-                    .frame(width: 25, height: 25)
-                    .padding(.trailing, 10)
-                Image(systemName: "message")
-                    .resizable()
-                    .frame(width: 25, height: 25)
-            }
-            .padding(5)
-            .frame(height: 50)
-            ScrollView(.vertical, showsIndicators: false) {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(MockData().stories) {
-                            StoryView(story: $0)
-                        }
+        ScrollView(.vertical, showsIndicators: false) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(MockData().stories) {
+                        StoryView(story: $0)
                     }
                 }
-                ForEach(MockData().posts) {
-                    PostView(post: $0, screenWidth: UIScreen.main.bounds.size.width)
-                }
+            }
+            ForEach(MockData().posts) {
+                PostView(post: $0, screenWidth: UIScreen.main.bounds.size.width)
             }
         }
+        .navigationBarTitle("", displayMode: .inline)
+        .toolbar(content: {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image("instagram")
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFit()
+                    .frame(width: 130)
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                HStack {
+                    Image(systemName: "plus.square.on.square")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .padding(.trailing, 10)
+                    Image(systemName: "message")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                }
+            }
+        })
     }
 }
 
